3f6c05ba2fa447cce8132ca6564f7f4ff25c91e7
William Siqueira <william.fatecsjc@gmail.com>
2021-06-21 21:52:27 -0300

    updating to latest version

--- leo-crawler/src/main/java/io/sjcdigital/Main.java
+++ leo-crawler/src/main/java/io/sjcdigital/Main.java
@@ -9,65 +9,57 @@ import org.slf4j.LoggerFactory;
 import io.sjcdigital.service.FileService;
 import picocli.CommandLine;
 
-@CommandLine.Command(mixinStandardHelpOptions = true, name="leo-crawler" )
-public class Main  implements Runnable {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+@CommandLine.Command(mixinStandardHelpOptions = true, name = "leo-crawler")
+public class Main implements Runnable {
 
-	@CommandLine.Option(names = {"-a", "--all"}, description = "Get all data, biomeas, states, regions", defaultValue = "false")
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    @CommandLine.Option(names = {"-a", "--all"}, description = "Get all data, biomeas, states, regions", defaultValue = "false")
     Boolean all;
-	
-	@CommandLine.Option(names = {"-s", "--state"}, description = "Get only the state data", defaultValue = "false")
+
+    @CommandLine.Option(names = {"-s", "--state"}, description = "Get only the state data", defaultValue = "false")
     Boolean state;
-	
-	@CommandLine.Option(names = {"-r", "--region"}, description = "Get only the region data", defaultValue = "false")
+
+    @CommandLine.Option(names = {"-r", "--region"}, description = "Get only the region data", defaultValue = "false")
     Boolean region;
-	
-	@CommandLine.Option(names = {"-b", "--biome"}, description = "Get only the biome data", defaultValue = "false")
+
+    @CommandLine.Option(names = {"-b", "--biome"}, description = "Get only the biome data", defaultValue = "false")
     Boolean biome;
-	
-	@Inject
-	@Named("state")
-	FileService stateService;
-	
-	@Inject
-	@Named("region")
-	FileService regionService;
-	
-	@Inject
-	@Named("biome")
-	FileService biomeService;
-	
-	@Override
-	public void run() {
-		
-		
-		if(all) {
-			
-			LOGGER.info("Getting all");
-			
-			stateService.run();
-			regionService.run();
-			biomeService.run();
-			
-		} else {
-			
-			if(state) { 
-				LOGGER.info("Getting State");
-				stateService.run(); 
-			}
-			
-			if(region) { 
-				LOGGER.info("Getting Region");
-				regionService.run(); 
-			}
-			
-			if(biome) { 
-				LOGGER.info("Getting Biome");
-				biomeService.run(); 
-			}
-			
-		}
-	}
+
+    @Inject
+    @Named("state")
+    FileService stateService;
+
+    @Inject
+    @Named("region")
+    FileService regionService;
+
+    @Inject
+    @Named("biome")
+    FileService biomeService;
+
+    @Override
+    public void run() {
+        if (all) {
+            LOGGER.info("Getting all");
+            stateService.run();
+            regionService.run();
+            biomeService.run();
+
+        } else {
+            if (state) {
+                LOGGER.info("Getting State");
+                stateService.run();
+            }
+            if (region) {
+                LOGGER.info("Getting Region");
+                regionService.run();
+            }
+            if (biome) {
+                LOGGER.info("Getting Biome");
+                biomeService.run();
+            }
+        }
+    }
 
 }
--- leo-crawler/src/main/java/io/sjcdigital/service/FileService.java
+++ leo-crawler/src/main/java/io/sjcdigital/service/FileService.java
@@ -5,142 +5,116 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import javax.enterprise.context.ApplicationScoped;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import io.sjcdigital.model.Constants;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public abstract class FileService {
-	
-	@ConfigProperty(name = "sufix.csv")
-	String sufixCSV;
-	
-	@ConfigProperty(name = "data.read.path")
-	String readDataPath;
-	
-	@ConfigProperty(name = "data.path")
-	String dataPath;
+
+    @ConfigProperty(name = "sufix.csv")
+    String sufixCSV;
+
+    @ConfigProperty(name = "data.read.path")
+    String readDataPath;
+
+    @ConfigProperty(name = "data.path")
+    String dataPath;
 
     Map<String, String> values;
     Map<String, String> latValues;
     Map<String, String> longValues;
-    
+
     abstract String getType();
+
     abstract String getURL();
 
     public void run() {
-    	getFileFromInpe();
-    	readLocalFilesAndParse();
+        getFileFromInpe();
+        readLocalFilesAndParse();
     }
-    
-	private void readLocalFilesAndParse() {
-		
-		StringBuilder dataFinal = new StringBuilder(Constants.HEADER);
-		dataFinal.append("\n");
 
-		try (Stream<Path> paths = Files.walk(Paths.get(readDataPath + getType()))) {
-			
-			paths.filter(Files::isRegularFile).forEach(
-			f -> {
-				
-				try (Stream<String> lines = Files.lines(f)) {
-					
-					dataFinal.append(createSummarizedFiles(lines, f.getFileName().toString()));
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			});
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
+    private void readLocalFilesAndParse() {
+        var dataFinal = new StringBuilder(Constants.HEADER);
+        dataFinal.append("\n");
+        try (var paths = Files.walk(Paths.get(readDataPath + getType()))) {
+            paths.filter(Files::isRegularFile).forEach(
+                                                       f -> {
+                                                           try (var lines = Files.lines(f)) {
+                                                               dataFinal.append(createSummarizedFiles(lines, f.getFileName().toString()));
+                                                           } catch (IOException e) {
+                                                               e.printStackTrace();
+                                                           }
+                                                       });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
             Files.write(Paths.get(dataPath + getType() + "_sumarized.csv"), dataFinal.toString().getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
-	}
-	
+    }
 
-	private String createSummarizedFiles(Stream<String> lines, String filename) {
-		
-		String typeKey = filename.replace(sufixCSV, "");
-		StringBuilder data = new StringBuilder();
-		
-		lines.forEach(l -> {
-			
-			if(l.matches("^([0-9]{4}).*$")) {
-				
-				for (int i = 0; i < l.split(",").length -1; i++) {
-					String str = l.split(",")[i];
-					
-					str = str.equals("-") ? "0" : str;
-					
-					data.append(str);
-					data.append(Constants.SEPARATOR);
-				}
-				
-				data.append(values.get(typeKey));
-				
-				data.append(Constants.SEPARATOR);
-				data.append(latValues.get(typeKey));
-				data.append(Constants.SEPARATOR);
-				data.append(longValues.get(typeKey));
-				
-				data.append("\n");
-			}
-				
-		});
-		
-		return data.toString();
-		
-	}
+    private String createSummarizedFiles(Stream<String> lines, String filename) {
+        var typeKey = filename.replace(sufixCSV, "");
+        var data = new StringBuilder();
+        lines.forEach(l -> {
+            if (l.matches("^([0-9]{4}).*$")) {
+                for (int i = 0; i < l.split(",").length - 1; i++) {
+                    var str = l.split(",")[i];
+                    str = str.equals("-") ? "0" : str;
+                    data.append(str);
+                    data.append(Constants.SEPARATOR);
+                }
 
-	private void getFileFromInpe() {
-		
-		values.forEach((k,v) -> {
-			createFiles(getURL() + k + sufixCSV, k + sufixCSV);
-		});
-		
-	}
-	
-	private void createFiles(final String url, final String fileName) {
-		
-		try {
-			
-			String name = readDataPath + getType() + "/";
-			var directory = new File(name);
-	        
-	        if (!directory.exists()) {
-	            directory.mkdirs();
-	        }
-			
-			ReadableByteChannel readChannel = Channels.newChannel(new URL(url).openStream());
-			FileOutputStream fileOS = new FileOutputStream(name + fileName);
-			FileChannel writeChannel = fileOS.getChannel();
-			writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
-			
-			writeChannel.close();
-			readChannel.close();
-			fileOS.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+                data.append(values.get(typeKey));
+                data.append(Constants.SEPARATOR);
+                data.append(latValues.get(typeKey));
+                data.append(Constants.SEPARATOR);
+                data.append(longValues.get(typeKey));
+                data.append("\n");
+            }
+
+        });
+        return data.toString();
+
+    }
+
+    private void getFileFromInpe() {
+        values.forEach((k, v) -> {
+            createFiles(getURL() + k + sufixCSV, k + sufixCSV);
+        });
+    }
+
+    private void createFiles(final String url, final String fileName) {
+        System.out.println("Creating file for URL " + url);
+        System.out.println("Saving to " + fileName);
+        try {
+            var name = readDataPath + getType() + "/";
+            var directory = new File(name);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            var readChannel = Channels.newChannel(new URL(url).openStream());
+            var fileOS = new FileOutputStream(name + fileName);
+            var writeChannel = fileOS.getChannel();
+            writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
+
+            writeChannel.close();
+            readChannel.close();
+            fileOS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
